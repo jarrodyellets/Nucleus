28cdabf299f68faa0e9b6f18a6d33083b1e6a2d0
jarrodyellets <jarrod524@gmail.com>
2019-01-08 14:55:50 +0100

    Removed like bottom on modal

--- packages/realmark-ui/src/components/addComment.js
+++ packages/realmark-ui/src/components/addComment.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactMarkdown from 'react-markdown';
+import CodeBlock from './markdown-render/codeblock';
+import CodeInline from './markdown-render/codeinline';
 import Post from './post';
 
 class AddComment extends Component {
@@ -12,6 +15,9 @@ class AddComment extends Component {
   }
 
   render(){
+    const now = Date.now();
+    let difference = now - this.props.post.date;
+    const minDifference = Math.floor(difference/1000/60);
     return (
       <div className="addCommentWrapper">
         <div className="addCommentBlackout">
@@ -21,7 +27,18 @@ class AddComment extends Component {
               <div className="addCommentClose"><i className="fas fa-times"></i></div>
             </div>
             <div className="addCommentBody">
-              <Post post={this.props.post} />
+              <div className="postWrapper">
+                <div className="postHeadingWrapper">
+                  <img className="postImg" alt="profile" src={this.props.post.imageURL ? this.props.post.imageURL : "https://www.jarrodyellets.com/images/profilePlaceholder.png"} />
+                  <div className="postHeadingTextWrapper">
+                    <div className="postHeadingTextFullName">{this.props.post.firstName} {this.props.post.lastName} <span className="postHeadingTextTime">{minDifference}m</span></div>
+                    <div className="postHeadingTextUserName">@{this.props.post.username}</div>
+                  </div>
+                </div>
+                <div className="postTextWrapper">
+                  <ReactMarkdown source={this.props.post.post} renderers={{code: CodeBlock, inlineCode: CodeInline}} className="postText" />
+                </div>
+              </div>
             </div>
             <div className="addCommentFooter">
               <div className="addCommentFooterTop">
