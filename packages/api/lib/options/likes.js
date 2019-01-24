c6dff46060e2cd885d365fad10998d6fe1beeacd
jarrodyellets <jarrod524@gmail.com>
2019-01-24 09:07:16 +0100

    nested dislikes

--- packages/api/lib/options/likes.js
+++ packages/api/lib/options/likes.js
@@ -11,13 +11,14 @@ exports.create = {
     let user = await client.users.query({id: request.params.userId});
     let path = request.payload.path
     const posts = user[0].posts;
+    let comment
     let postIndex = await posts.findIndex(x => x.postID == request.params.postId);
     const parentPost = await posts.findIndex(p => p.postID == path[0]);
     if (await postIndex === -1){
-      let comment = await findComment(posts, path);
-      await console.log(comment);
+      comment = await findComment(posts, path);
       await comment.likes.push(request.auth.credentials.id);
     } else {
+      comment = user[0].posts[parentPost];
       await posts[postIndex].likes.push(request.auth.credentials.id);
     }
     await client.users.update({id: request.params.userId, posts})
@@ -25,7 +26,7 @@ exports.create = {
     const signedUser = await client.users.query({id: request.auth.credentials.id})
     const timeline = await createTimeline(request.auth.credentials.id);
     return {
-      posts: user[0].posts, post: user[0].posts[parentPost], signedPosts: signedUser[0].posts, timeline
+      posts: user[0].posts, post: comment, signedPosts: signedUser[0].posts, timeline
     };
   }
 }
@@ -34,16 +35,25 @@ exports.delete = {
   handler: async (request, h) => {
     const client = returnClient();
     let user = await client.users.query({id: request.params.userId});
+    let path = request.payload.path
     const posts = user[0].posts;
-    const postIndex = await posts.findIndex(x => x.postID == request.params.postId);
-    const likeIndex = await posts[postIndex].likes.find(like => like == request.auth.credentials.id);
-    await posts[postIndex].likes.splice(likeIndex, 1);
-    await client.users.update({id: request.params.userId, posts});
-    user = await client.users.query({id: request.params.userID});
+    let comment
+    let postIndex = await posts.findIndex(x => x.postID == request.params.postId);
+    const parentPost = await posts.findIndex(p => p.postID == path[0]);
+    if (await postIndex === -1){
+      comment = await findComment(posts, path);
+    } else {
+      comment = user[0].posts[parentPost];
+    }
+    const likeIndex = await comment.likes.find(like => like == request.auth.credentials.id);
+    await comment.likes.splice(likeIndex, 1);
+    await client.users.update({id: request.params.userId, posts})
+    user = await client.users.query({id: request.params.userId})
     const signedUser = await client.users.query({id: request.auth.credentials.id})
     const timeline = await createTimeline(request.auth.credentials.id);
     return {
-      posts: user[0].posts, post: user[0].posts[postIndex], signedPosts: signedUser[0].posts, timeline
+      posts: user[0].posts, post: comment, signedPosts: signedUser[0].posts, timeline
     };
   }
 }
+
