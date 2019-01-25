84f0218f0a9fdedf4179e3a3066552876bdad521
jarrodyellets <jarrod524@gmail.com>
2019-01-25 10:15:50 +0100

    Nested dislikes

--- packages/realmark-ui/src/actions/likeAction.js
+++ packages/realmark-ui/src/actions/likeAction.js
@@ -1,6 +1,6 @@
 import { LIKE } from './types';
 
-export const addLike = (userID, postID, path, currentPost) => dispatch => {
+export const addLike = (userID, postID, path) => dispatch => {
   return new Promise((resolve, reject) => {
     fetch('http://localhost:8000/users/' + userID + '/posts/' + postID + '/likes', {
       method: 'POST',
--- packages/realmark-ui/src/components/displayComment.js
+++ packages/realmark-ui/src/components/displayComment.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Comment from './comment';
 import LikesContainer from './likesContainer';
-import { addLike } from '../actions/likeAction';
+import { addLike, disLike } from '../actions/likeAction';
 
 
 class DisplayComment extends Component {
@@ -10,6 +10,7 @@ class DisplayComment extends Component {
     super(props);
 
     this.handleLike = this.handleLike.bind(this);
+    this.handleDislike = this.handleDislike.bind(this);
 
   }
 
@@ -18,7 +19,10 @@ class DisplayComment extends Component {
     this.props.addLike(userID, postID, path);
   }
 
-
+  handleDislike(event, userID, postID, path){
+    event.stopPropagation();
+    this.props.disLike(userID, postID, path);
+  }
 
   render(){
     const comments = this.props.comments.sort(function(a, b){
@@ -28,7 +32,7 @@ class DisplayComment extends Component {
       return (
         <div className="commentsMainWrapper" key={this.props.id} onClick={() => {this.props.handleModal(true, comment); this.props.handleSelectedPost(comment.postID)}}>
           <Comment comment={comment} handleLike={this.props.handleLike} currentPost={this.props.currentPost} handleTrigger={this.props.handleTrigger} handleSearch={this.props.handleSearch} />
-          <LikesContainer post={comment} id={this.props.user.id} handleLike={this.handleLike} handleTrigger={this.props.handleTrigger} />
+          <LikesContainer post={comment} id={this.props.user.id} handleLike={this.handleLike} handleDislike={this.handleDislike} handleTrigger={this.props.handleTrigger} />
         </div>
       )
     })
@@ -45,4 +49,4 @@ const mapStateToProps = state => ({
   currentPost: state.trigger.currentPost
 })
 
-export default connect(mapStateToProps, {addLike})(DisplayComment);
+export default connect(mapStateToProps, {addLike, disLike})(DisplayComment);
