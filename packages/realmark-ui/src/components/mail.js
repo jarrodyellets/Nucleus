db643986985f8043edc1a100c0374c2d73f0da56
jarrodyellets <jarrod524@gmail.com>
2019-02-28 10:10:44 +0100

    Show sent mail

--- packages/realmark-ui/src/components/mail.js
+++ packages/realmark-ui/src/components/mail.js
@@ -5,37 +5,41 @@ import Message from './message';
 class Mail extends Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       received: true
-    }
+    };
+
+    this.handleReceived = this.handleReceived.bind(this);
+  }
+
+  handleReceived(received) {
+    this.setState({
+      received
+    });
   }
   render() {
-    console.log(this.props.user);
-    const mail = this.props.user.mail.received.map((message) => {
-      return (
-        <Message subject={message.subject} author={message.from} date={message.date} />
-      )
-    })
+    const mail = this.state.received ? this.props.user.mail.received : this.props.user.mail.sent
+    const messages = mail.map(message => {
+      return <Message message={message} received={this.state.received}/>;
+    });
     return (
       <div className="mainWrapper">
         <div className="mailInnerWrapper">
           <div className="mailHeader">
-            <div className="mailHeaderReceived">
+            <div className={this.state.received ? 'mailHeaderActive' : 'mailHeaderOff'} onClick={() => {this.handleReceived(true)}}>
               <i class="fas fa-inbox" /> Inbox
             </div>
-            <div className="mailHeaderSent">
+            <div className={this.state.received ? 'mailHeaderOff' : 'mailHeaderActive'} onClick={() => {this.handleReceived(false)}}>
               <i class="far fa-paper-plane" /> Sent
             </div>
           </div>
           <div className="mailBoxWrapper">
             <div className="mailBoxHeader">
               <div className="mailBoxSubject mailBoxHeaderText">Subject</div>
-              <div className="mailBoxAuthor mailBoxHeaderText">From</div>
+              <div className="mailBoxAuthor mailBoxHeaderText">{this.state.received ? "From" : "To"}</div>
               <div className="mailBoxDate mailBoxHeaderText">Date</div>
             </div>
-            <div className="mailBoxBody">
-              {mail}
-            </div>
+            <div className="mailBoxBody">{messages}</div>
           </div>
         </div>
       </div>
@@ -45,6 +49,6 @@ class Mail extends Component {
 
 const mapStateToProps = state => ({
   user: state.user
-})
+});
 
 export default connect(mapStateToProps)(Mail);
--- packages/realmark-ui/src/components/message.js
+++ packages/realmark-ui/src/components/message.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Message = props => {
-  const date = new Date(props.date);
+  const date = new Date(props.message.date);
   return (
     <div className="messageWrapper">
       <div className="messageBody">
-        <div className="messageSubject">{props.subject}</div>
-        <div className="messageAuthor">{props.author}</div>
+        <div className="messageSubject">{props.message.subject}</div>
+        <div className="messageAuthor">{props.received ? props.message.from : props.message.to}</div>
         <div className="messageDate">{date.toLocaleString('en-US', { hour12: false }).replace(/,/g, ' ')}</div>
       </div>
     </div>
