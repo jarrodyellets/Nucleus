718acb18d5eba96184396417ccfa4afe2bff62b9
jarrodyellets <jarrod524@gmail.com>
2019-02-28 09:55:42 +0100

    format mail date

--- packages/realmark-ui/src/components/message.js
+++ packages/realmark-ui/src/components/message.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Message = (props) => {
+const Message = props => {
+  const date = new Date(props.date);
   return (
     <div className="messageWrapper">
       <div className="messageBody">
         <div className="messageSubject">{props.subject}</div>
         <div className="messageAuthor">{props.author}</div>
-        <div className="messageDate">{props.date}</div>
+        <div className="messageDate">{date.toLocaleString('en-US', { hour12: false }).replace(/,/g, ' ')}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Message;
